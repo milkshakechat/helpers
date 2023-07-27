884805371acec6cbaa6464334e28a1f1b60f7b1c
0xterran <0xterran@protonmail.com>
2023-07-28 06:17:28 +0700

    cash out escrow to main wallet

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,13 +3,19 @@ import { TimestampFirestore } from "../types/base.types";
 export const RECALL_GRACE_PERIOD_DAYS = 90;
 export const checkIfRecallable = (firestoreTimestamp: TimestampFirestore) => {
   const now = Date.now();
-  console.log("now", now);
-  console.log("firestoreTimestamp", firestoreTimestamp);
   const timestamp = (firestoreTimestamp as any).seconds * 1000;
-  console.log("timestamp", timestamp);
+
   const diff = now - timestamp;
   const remaining = 1000 * 60 * 60 * 24 * RECALL_GRACE_PERIOD_DAYS;
-  console.log(`time diff = ${diff} vs remaining = ${remaining}`);
+
   const recallable = diff < remaining;
   return recallable;
+};
+export const checkIfCashOutAble = (firestoreTimestamp: TimestampFirestore) => {
+  const now = Date.now();
+  const timestamp = (firestoreTimestamp as any).seconds * 1000;
+  const days = 1000 * 60 * 60 * 24 * RECALL_GRACE_PERIOD_DAYS;
+  const total = timestamp + days;
+  const cashOutAble = total < now;
+  return cashOutAble;
 };
